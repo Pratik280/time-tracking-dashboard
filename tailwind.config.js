30e88ded2c2b4f87e80c8112fbb7cb301e94c133
Pratik280 <pratikchandlekar280@gmail.com>
2022-02-17 20:07:27 +0530

    setup and folder structure

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  content: ["./dist/index.html"],
+  theme: {
+    extend: {
+      colors: {
+        "clr-blue": "hsl(246, 80%, 60%)",
+
+        "clr-light-red-work": "hsl(15, 100%, 70%)",
+        "clr-soft-blue-play": "hsl(195, 74%, 62%)",
+        "clr-light-red-study": "hsl(348, 100%, 68%)",
+        "clr-lime-green-exercise": "hsl(145, 58%, 55%)",
+        "clr-violet-social": -"hsl(264, 64%, 52%)",
+        "clr-soft-orange-self-care": "hsl(43, 84%, 65%)",
+
+        "clr-very-dark-blue": "hsl(226, 43%, 10%)",
+        "clr-dark-blue": "hsl(235, 46%, 20%)",
+        "clr-desaturated-blue": "hsl(235, 45%, 61%)",
+        "clr-pale-Blue": "hsl(236, 100%, 87%)",
+      },
+    },
+  },
+  plugins: [],
+};
