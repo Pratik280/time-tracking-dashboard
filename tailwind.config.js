6a4c7be1b170dad50b32c02cf0cdf1d09b92b899
Pratik280 <pratikchandlekar280@gmail.com>
2022-02-18 21:02:37 +0530

    styled profile/hero component

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        "clr-blue": "hsl(246, 80%, 60%)",
+        "clr-blue": "hsl(246, 80%, 60%)", // jeremy
 
         "clr-light-red-work": "hsl(15, 100%, 70%)",
         "clr-soft-blue-play": "hsl(195, 74%, 62%)",
@@ -12,10 +12,10 @@ module.exports = {
         "clr-violet-social": -"hsl(264, 64%, 52%)",
         "clr-soft-orange-self-care": "hsl(43, 84%, 65%)",
 
-        "clr-very-dark-blue": "hsl(226, 43%, 10%)",
-        "clr-dark-blue": "hsl(235, 46%, 20%)",
-        "clr-desaturated-blue": "hsl(235, 45%, 61%)",
-        "clr-pale-Blue": "hsl(236, 100%, 87%)",
+        "clr-very-dark-blue": "hsl(226, 43%, 10%)", // bg
+        "clr-dark-blue": "hsl(235, 46%, 20%)", // hover accent
+        "clr-desaturated-blue": "hsl(235, 45%, 61%)", // text
+        "clr-pale-blue": "hsl(236, 100%, 87%)", // text main
       },
     },
   },
