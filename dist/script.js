226808106429b247adb78baf7b82ab2196b7f3c4
Pratik280 <pratikchandlekar280@gmail.com>
2022-03-15 20:39:30 +0530

    js logic

--- dist/script.js
+++ dist/script.js
@@ -1,20 +1,56 @@
-"use strict";
-
 function insertDataInHtml(data) {
-  const timeFramesInHtml = document.querySelector("#time-frames").querySelectorAll("button")
-  const allCards = document.querySelectorAll(".card")
-  const firstCard = document.querySelector(".card")
-  timeFramesInHtml.forEach(element => {
+  // grabbing three (daily, weekly, monthly) buttons
+  const timeFramesInHtml = document
+    .querySelector("#time-frames")
+    .querySelectorAll("button");
+  // grabbing all cards
+  const allCards = document.querySelectorAll(".card");
+
+  // on initial load shows data of daily
+  // for each card section grab heading h1 for its dataset which matches title in json data for getting desired object
+  // grabbing two spans for displaying hours and last week data
+  allCards.forEach((card) => {
+    const headingHtml = card.querySelector("h1");
+    const hoursHtml = card.querySelector(".hours").querySelector("span");
+    const previousHtml = card.querySelector(".previous").querySelector("span");
+    let currentObj = data.filter(
+      (i) => i.title === headingHtml.dataset.heading.trim()
+    );
+    hoursHtml.innerHTML = currentObj[0].timeframes.daily.current;
+    previousHtml.innerHTML = currentObj[0].timeframes.daily.previous;
+  });
+
+  // onclicking (daily, weekly, monthly) button
+  // adding text-neutral-100 class to currently clicked and removing same class from other btns
+  timeFramesInHtml.forEach((element) => {
     element.addEventListener("click", (e) => {
-      allCards.forEach(card => {
-        const headingHtml = card.querySelector("h1").innerText
-        const hoursHtml = card.querySelector(".hours").querySelector("span")
-        const previousHtml = card.querySelector(".previous").querySelector("span")
-        let currentObj = data.filter(i => i.title===headingHtml)
-        hoursHtml.innerHTML = currentObj[0].timeframes[element.dataset.timeframe].current
-        previousHtml.innerHTML = currentObj[0].timeframes[element.dataset.timeframe].previous
-      })
-    })
+      element.classList.toggle("text-clr-desaturated-blue");
+      element.classList.toggle("text-neutral-100");
+      timeFramesInHtml.forEach((i) => {
+        if (i !== e.target) {
+          if (i.classList.contains("text-neutral-100")) {
+            i.classList.remove("text-neutral-100");
+            i.classList.add("text-clr-desaturated-blue");
+          }
+        }
+      });
+      // for each card section grab heading h1 for its dataset which matches title in json data for getting desired object
+      // grabbing two spans for displaying hours and last week data
+      allCards.forEach((card) => {
+        const headingHtml = card.querySelector("h1");
+        const hoursHtml = card.querySelector(".hours").querySelector("span");
+        const previousHtml = card
+          .querySelector(".previous")
+          .querySelector("span");
+        let currentObj = data.filter(
+          (i) => i.title === headingHtml.dataset.heading.trim()
+        );
+        hoursHtml.innerHTML =
+          currentObj[0].timeframes[element.dataset.timeframe].current;
+        previousHtml.innerHTML =
+          currentObj[0].timeframes[element.dataset.timeframe].previous;
+      });
+    });
   });
 }
 
@@ -34,9 +70,7 @@ function fetchData(url) {
     });
 }
 
-const url = "./data.json";
-fetchData(url);
-
-// TODO
-// 1. onCLick btn bold white class 
-// 2. initial show content for daily
+window.onload = function () {
+  const url = "./data.json";
+  fetchData(url);
+};
